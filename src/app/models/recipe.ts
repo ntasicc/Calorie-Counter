2231e53cc548c9a6fc03de3caa7ad6be53db98b0
ntasicc <nikols.tt@elfak.rs>
2021-07-29 20:16:50 +0200

    Model for recipe

---
+++ src/app/models/recipe.ts
@@ -0,0 +1,12 @@
+export interface Recipe {
+  id: number;
+  name: string;
+  image: string;
+  summary: string;
+  description: string;
+  bookmark: string;
+  calories: string;
+  carbs: string;
+  fat: string;
+  protein: string;
+}
