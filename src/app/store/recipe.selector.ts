e40f81151958dcf4036ab1527a6d2708ea2959cc
ntasicc <nikols.tt@elfak.rs>
2021-07-29 20:30:15 +0200

    Selectors for recipes and one specific recipe

---
+++ src/app/store/recipe.selector.ts
@@ -0,0 +1,33 @@
+import { createSelector } from '@ngrx/store';
+import { Recipe } from '../models/recipe';
+import { AppState } from './app.state';
+import { RecipeState } from './recipe.reducer';
+
+export const selectRecipeFeature = createSelector(
+  (state: AppState) => state.recipes,
+  (recipes) => recipes
+);
+
+export const selectAllRecipes = createSelector(
+  selectRecipeFeature,
+  (state: RecipeState) =>
+    Object.values(state.entities)
+      .filter((recipe) => recipe != null)
+      .map((recipe) => <Recipe>recipe)
+);
+
+export const selectAllRecipesAsDict = createSelector(
+  selectRecipeFeature,
+  (state: RecipeState) => state.entities
+);
+
+export const selectIDOfOneRecipe = createSelector(
+  selectRecipeFeature,
+  (state: RecipeState) => state.selectedRecipeID
+);
+
+export const selectOneRecipeBasedOnID = createSelector(
+  selectAllRecipes,
+  selectIDOfOneRecipe,
+  (recipes, recipeID) => recipes[recipeID] ?? null
+);
