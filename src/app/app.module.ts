f17dd1ddc1663fd1cc076abc929475ef79f1f129
ntasicc <nikols.tt@elfak.rs>
2021-07-29 20:14:32 +0200

    Recipe and Igridient reducer and effect module added

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { recipeReducer } from './store/recipe.reducer';
+import { RecipeEffect } from './store/recipe.effects';
+import { ingredientReducer } from './store/ingredient.reducer';
+import { IngredientEffect } from './store/ingredient.effects';
+
+@NgModule({
+  declarations: [AppComponent, RecipeCardComponent, RecipeListComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot({
+      recipes: recipeReducer,
+      ingredients: ingredientReducer,
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+    }),
+    EffectsModule.forRoot([RecipeEffect, IngredientEffect]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
