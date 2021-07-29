b94725bc3399a68b136d4413ac0da7d021431e63
ntasicc <nikols.tt@elfak.rs>
2021-07-29 20:32:38 +0200

    Reducer for recipes

---
+++ src/app/store/recipe.reducer.ts
@@ -0,0 +1,25 @@
+import { createEntityAdapter, EntityState } from '@ngrx/entity';
+import { createReducer, on } from '@ngrx/store';
+import { Recipe } from '../models/recipe';
+import * as RecipeActions from './recipe.actions';
+
+export interface RecipeState extends EntityState<Recipe> {
+  selectedRecipeID: number;
+}
+
+const adapter = createEntityAdapter<Recipe>();
+
+const initialState: RecipeState = adapter.getInitialState({
+  selectedRecipeID: -1,
+});
+
+export const recipeReducer = createReducer(
+  initialState,
+  on(RecipeActions.loadAllRecipesSuccess, (state, { recipes }) =>
+    adapter.setAll(recipes, state)
+  ),
+  on(RecipeActions.selectRecipe, (state, { recipeID }) => ({
+    ...state,
+    selectedRecipeID: recipeID,
+  }))
+);
