dc111a39cd03f4cba2e1d664ae4fe95fe2eb817d
ntasicc <nikols.tt@elfak.rs>
2021-07-30 17:02:32 +0200

    List of all recipes from DB

---
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { Ingredient } from 'src/app/models/ingredient';
+import { Recipe } from 'src/app/models/recipe';
+import { AppState } from 'src/app/store/app.state';
+import * as RecipeActions from '../../store/recipe.actions';
+import * as IngredientActions from '../../store/ingredient.actions';
+import {
+  selectAllRecipes,
+  selectOneRecipeBasedOnID,
+} from '../../store/recipe.selector';
+import { selectAllIngredients } from 'src/app/store/ingredient.selectors';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Observable<readonly Recipe[]> = of([]);
+  selectedRecipe: Observable<Recipe> = of();
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(RecipeActions.loadAllRecipes());
+    this.recipes = this.store.select(selectAllRecipes);
+  }
+
+  selectSpecificRecipe(recipe: Recipe) {
+    if (recipe) {
+      this.store.dispatch(RecipeActions.selectRecipe({ recipeID: recipe.id }));
+    }
+  }
+}
