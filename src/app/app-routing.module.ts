3a537241e766811f4b7ffbbcf27e086efd973748
ntasicc <nikols.tt@elfak.rs>
2021-07-31 19:02:51 +0200

    Routes added for recipe list and ingredient list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngredientListComponent } from './components/ingredient-list/ingredient-list.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RecipeListComponent,
+  },
+  {
+    path: 'ingredients',
+    component: IngredientListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
