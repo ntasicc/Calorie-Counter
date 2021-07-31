6f5af1b6e7a2006479fc8b651d58b98d757c4e61
ntasicc <nikols.tt@elfak.rs>
2021-07-31 19:06:21 +0200

    Store subscription for selected recipe

--- src/app/components/ingredient-list/ingredient-list.component.ts
+++ src/app/components/ingredient-list/ingredient-list.component.ts
@@ -6,6 +6,7 @@ import { Recipe } from 'src/app/models/recipe';
 import { AppState } from 'src/app/store/app.state';
 import * as IngredientAction from '../../store/ingredient.actions';
 import { selectAllIngredients } from '../../store/ingredient.selectors';
+import { selectOneRecipeBasedOnID } from '../../store/recipe.selector';
 
 @Component({
   selector: 'app-ingredient-list',
@@ -13,12 +14,15 @@ import { selectAllIngredients } from '../../store/ingredient.selectors';
   styleUrls: ['./ingredient-list.component.css'],
 })
 export class IngredientListComponent implements OnInit {
-  @Input() selectedRecipe: Recipe | undefined;
+  selectedRecipe: Recipe | undefined;
   ingredientsForRecipe: Observable<Ingredient[]> = of([]);
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store.select(selectOneRecipeBasedOnID).subscribe((recipe) => {
+      this.selectedRecipe = recipe;
+    });
     if (this.selectedRecipe) {
       this.store.dispatch(
         IngredientAction.loadIngredientsOfSpecificRecipe({
