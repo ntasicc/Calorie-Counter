6bd40d0910fd1acf1e03ff4d7dedc944a4b5e177
ntasicc <nikols.tt@elfak.rs>
2021-08-03 18:10:07 +0200

    Auth state added

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,7 +1,9 @@
+import { AuthState } from './auth.reducer';
 import { IngredientState } from './ingredient.reducer';
 import { RecipeState } from './recipe.reducer';
 
 export interface AppState {
   recipes: RecipeState;
   ingredients: IngredientState;
+  user: AuthState;
 }
