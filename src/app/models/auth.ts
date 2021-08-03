16e127ea2591ac69f7ff3f1242803d7d3e129919
ntasicc <nikols.tt@elfak.rs>
2021-08-03 18:09:04 +0200

    User model added

---
+++ src/app/models/auth.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  allTimeCalories: string;
+  allTimeCarbs: string;
+  allTimeFat: string;
+  allTimeProtein: string;
+}
