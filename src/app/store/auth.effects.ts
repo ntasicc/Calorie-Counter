2444dccf00ff0bd7d39d84ab022e48e11aa4c8cf
ntasicc <nikols.tt@elfak.rs>
2021-08-05 15:02:46 +0200

    Added effects for adding and removing user in local storage

--- src/app/store/auth.effects.ts
+++ src/app/store/auth.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { catchError, map, mergeMap } from 'rxjs/operators';
+import { catchError, map, mergeMap, tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 import * as AuthActions from './auth.actions';
 
@@ -19,5 +19,26 @@ export class AuthtEffect {
         )
       )
     )
+  );
+
+  removeFromLocalStorage$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.logoutUser),
+        tap(() => {
+          localStorage.removeItem('user');
+        })
+      ),
+    { dispatch: false }
+  );
+  addToLocalStorage$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.loginUserSuccess),
+        tap((action) =>
+          localStorage.setItem('user', JSON.stringify(action.user))
+        )
+      ),
+    { dispatch: false }
   );
 }
