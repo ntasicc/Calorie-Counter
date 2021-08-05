343bf1661ae2764fe5bc068cc4b77be5b294531e
ntasicc <nikols.tt@elfak.rs>
2021-08-05 15:04:53 +0200

    Added profile view and logout functions if user is in local storage

--- src/app/components/top-nav-bar/top-nav-bar.component.ts
+++ src/app/components/top-nav-bar/top-nav-bar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.state';
+import { logoutUser } from '../../store/auth.actions';
 
 @Component({
   selector: 'app-top-nav-bar',
   templateUrl: './top-nav-bar.component.html',
-  styleUrls: ['./top-nav-bar.component.css']
+  styleUrls: ['./top-nav-bar.component.css'],
 })
 export class TopNavBarComponent implements OnInit {
-
-  constructor() { }
+  isLoggedIn: boolean = false;
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    let userSignedIn = localStorage.getItem('user');
+    console.log(userSignedIn);
+    if (userSignedIn) this.isLoggedIn = true;
   }
 
+  logout() {
+    this.store.dispatch(logoutUser());
+  }
 }
