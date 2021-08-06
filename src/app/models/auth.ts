445030dd238266c1bfb9e43acb92fe17dd424d6b
ntasicc <nikols.tt@elfak.rs>
2021-08-06 13:59:02 +0200

    User model now contains bookmarked recipes

--- src/app/models/auth.ts
+++ src/app/models/auth.ts
@@ -4,6 +4,7 @@ export interface User {
   lastName: string;
   username: string;
   password: string;
+  bookmarked: Array<number>;
   allTimeCalories: string;
   allTimeCarbs: string;
   allTimeFat: string;
