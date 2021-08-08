946228f6a5698be6bfb3885cc4408db0aaa620b4
ntasicc <nikols.tt@elfak.rs>
2021-08-08 19:16:10 +0200

    Action to keep track of what you ate

--- src/app/store/auth.actions.ts
+++ src/app/store/auth.actions.ts
@@ -1,5 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { User } from '../models/auth';
+import { Recipe } from '../models/recipe';
 
 export const loginUser = createAction(
   'Login user',
@@ -12,3 +13,16 @@ export const loginUserSuccess = createAction(
 );
 
 export const logoutUser = createAction('Logout user', props<{ user: User }>());
+
+export const updateUserInLocalStorage = createAction(
+  'Update user in local storage',
+  props<{ user: User }>()
+);
+
+export const eatThisMeal = createAction(
+  'Eat this meal',
+  props<{
+    recipe: Recipe;
+    user: User;
+  }>()
+);
