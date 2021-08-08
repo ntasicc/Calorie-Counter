7d10ddfb84edf63e7862ee38f29e6e74fa8a36c9
ntasicc <nikols.tt@elfak.rs>
2021-08-08 19:18:15 +0200

    Bookmark function moved from recipe reducer

--- src/app/store/auth.reducer.ts
+++ src/app/store/auth.reducer.ts
@@ -2,6 +2,7 @@ import { createEntityAdapter, EntityState } from '@ngrx/entity';
 import { createReducer, on } from '@ngrx/store';
 import { User } from '../models/auth';
 import * as AuthActions from './auth.actions';
+import * as RecipeActions from './recipe.actions';
 
 export interface AuthState extends EntityState<User> {
   user: User;
@@ -17,10 +18,10 @@ const initialState: AuthState = adapter.getInitialState({
     username: '',
     password: '',
     bookmarked: [],
-    allTimeCalories: '',
-    allTimeCarbs: '',
-    allTimeFat: '',
-    allTimeProtein: '',
+    allTimeCalories: 0,
+    allTimeCarbs: 0,
+    allTimeFat: 0,
+    allTimeProtein: 0,
   },
 });
 
@@ -29,6 +30,23 @@ export const AuthReducer = createReducer(
   on(AuthActions.loginUserSuccess, (state, { user }) =>
     adapter.setOne(user, state)
   ),
+  on(AuthActions.updateUserInLocalStorage, (state, { user }) => {
+    console.log('REDUCER');
+    localStorage.setItem('user', JSON.stringify(user));
+    return state;
+  }),
+  on(RecipeActions.changeBookmark, (state, { recipe, newValue }) => {
+    let user = <User>JSON.parse(<string>localStorage.getItem('user'));
+    if (newValue === 'yes') user.bookmarked.push(recipe.id);
+    else {
+      let index = user.bookmarked.indexOf(recipe.id);
+      user.bookmarked.splice(index, 1);
+    }
+    localStorage.setItem('user', JSON.stringify(user));
+    // recipe.bookmark = newValue;
+    // return adapter.setOne(recipe, state);
+    return adapter.setOne(user, state);
+  }),
   on(AuthActions.logoutUser, (state) => ({
     ...state,
     user: {
@@ -38,10 +56,10 @@ export const AuthReducer = createReducer(
       username: '',
       password: '',
       bookmarked: [],
-      allTimeCalories: '',
-      allTimeCarbs: '',
-      allTimeFat: '',
-      allTimeProtein: '',
+      allTimeCalories: 0,
+      allTimeCarbs: 0,
+      allTimeFat: 0,
+      allTimeProtein: 0,
     },
   }))
 );
